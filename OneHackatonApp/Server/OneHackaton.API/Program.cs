f1d7a0b859aa80e3e14c9bc600f59a3167aaf31d
Jakub <jakub.wrona97@gmail.com>
2022-05-27 23:23:13 +0200

    Added infrastructure registration, Jwt configuration

--- OneHackatonApp/Server/OneHackaton.API/Program.cs
+++ OneHackatonApp/Server/OneHackaton.API/Program.cs
@@ -1,15 +1,44 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 using OneHackaton.Infrastructure;
+using OneHackaton.Infrastructure.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager Configuration = builder.Configuration;
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 // Add services to the container.
 builder.Services.AddOneHackatonServiceRegistration(Configuration);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setup =>
+{
+    // Include 'SecurityScheme' to use JWT Authentication
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Name = "JWT Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, Array.Empty<string>() }
+    });
+}); 
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -20,6 +49,15 @@ builder.Services.AddAuthentication(options =>
     options.Audience = "https://OneHackaton.com";
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        corsBuilder =>
+        {
+            corsBuilder.AllowAnyOrigin().AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -29,8 +67,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors();
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<GetUserContextMiddleware>(connectionString);
 
 app.UseAuthorization();
 
