fcd5024bfd250b05e24bd1db5519d47872aa9a56
Jakub <jakub.wrona97@gmail.com>
2022-05-27 21:32:16 +0200

    Auth0 added

--- OneHackatonApp/Server/OneHackaton.API/Program.cs
+++ OneHackatonApp/Server/OneHackaton.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = "https://dev-cxcbcq2e.us.auth0.com/";
+    options.Audience = "https://OneHackaton.com";
+});
+
 
 var app = builder.Build();
 
