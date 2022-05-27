509bad031ac600ff3d7ec988d4b6ffb1f990d6a9
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-27 20:41:09 +0200

    Create Team.cs

---
+++ OneHackatonApp/Server/OneHackaton.Domain/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneHackaton.Domain
+{
+    public class Team
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
