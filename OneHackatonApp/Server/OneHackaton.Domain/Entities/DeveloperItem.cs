be5bfdefd71f84b70f5b41527c2c6bc3b659ece1
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-28 00:46:37 +0200

    Create DeveloperItem.cs

---
+++ OneHackatonApp/Server/OneHackaton.Domain/Entities/DeveloperItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneHackaton.Domain.Entities
+{
+    public class DeveloperItem
+    {
+        public int Id { get; set; }
+        public string Credentials { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset Date { get; set; } = DateTimeOffset.Now;
+        public int DeveloperId { get; set; }
+        public virtual Developer Developer{ get; set; }
+        public int TimeLineId { get; set; }
+        public virtual Timeline Timeline { get; set; }
+    }
+}
