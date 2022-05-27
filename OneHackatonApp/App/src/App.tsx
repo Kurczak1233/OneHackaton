d85161d9b31d61cdaaf58907bc18f618593b75b7
Jakub <jakub.wrona97@gmail.com>
2022-05-28 01:19:55 +0200

    Added auth0 logic

--- OneHackatonApp/App/src/App.tsx
+++ OneHackatonApp/App/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import MainPage from "./Pages/MainPage/MainPage";
+import AppLogic from "./AppLogic";
 
 export interface IPresent {
   value: number;
@@ -10,6 +11,10 @@ export interface IPresent {
 export const PresentContext = React.createContext<IPresent[]>([]);
 
 function App() {
+
+  const { checkIfRouteIsAuthenticated } = AppLogic();
+
+
   return <MainPage />;
 }
 
