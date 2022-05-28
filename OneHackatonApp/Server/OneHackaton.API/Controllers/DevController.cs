f84ce16c78b2b29e5fa34ea53c0f223502898abc
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-28 02:23:46 +0200

    Create DevController.cs

---
+++ OneHackatonApp/Server/OneHackaton.API/Controllers/DevController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneHackaton.Domain.DTOs;
+
+namespace OneHackaton.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DevController : Controller
+    {
+        [HttpPost]
+        public void DevPost([FromBody] PostDTO postDTO)
+        {
+
+        }
+    }
+}
