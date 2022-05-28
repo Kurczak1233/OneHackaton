53ebefd000de7de7ed5a9e5eb9e0c41d775b69c6
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-28 04:48:24 +0200

    DevRepository

---
+++ OneHackatonApp/Server/OneHackaton.Infrastructure/Repositories/DevItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using OneHackaton.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneHackaton.Infrastructure.Repositories
+{
+    public class DevItemRepository : BaseRepository<DeveloperItem>, IDevRepository
+    {
+        public DevItemRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
