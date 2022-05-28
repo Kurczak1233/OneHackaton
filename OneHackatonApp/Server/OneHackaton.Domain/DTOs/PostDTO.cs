aac11c8202f2db409dfa90685677c22f4a610a03
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-28 02:23:42 +0200

    Create PostDTO.cs

---
+++ OneHackatonApp/Server/OneHackaton.Domain/DTOs/PostDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneHackaton.Domain.DTOs
+{
+    public class PostDTO
+    {
+        public string Credentials { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+    }
+}
