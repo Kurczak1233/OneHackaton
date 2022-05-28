15f5c377463aea1a02a308e5060befc685a513d7
MikoloD <grygiel.mikolaj@gmail.com>
2022-05-28 02:41:06 +0200

    Create ClientController.cs

---
+++ OneHackatonApp/Server/OneHackaton.API/Controllers/ClientController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneHackaton.Domain.DTOs;
+
+namespace OneHackaton.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : Controller
+    {
+        [HttpPost]
+        public IActionResult UserPost([FromBody] PostDTO postDTO)
+        {
+            return Ok();
+        }
+    }
+}
